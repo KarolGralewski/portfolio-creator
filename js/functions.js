0524fc2ccd4c050cc4c7e86b16b26bf6d8388065
KarolGral <gralewski.karol@gmail.com>
2022-05-28 11:55:47 +0200

    Added limits for the input values

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,37 @@
-
 //fuction that copies the div and pastes it below
 //and adds number to the div id
 var projectsCounter = 1;
 function copyDiv() {
-   if (projectsCounter < 3) {
-      var container = document.getElementById('projects');
-      var section = document.getElementById("project0");
-      var clone = section.cloneNode(true);
-      clone.id = "project" + projectsCounter;
-      clone.name = "project" + projectsCounter;
-      projectsCounter++;
-      container.appendChild(clone);
-   }
-
+  if (projectsCounter < 3) {
+    var container = document.getElementById("projects");
+    var section = document.getElementById("project0");
+    var clone = section.cloneNode(true);
+    clone.id = "project" + projectsCounter;
+    clone.name = "project" + projectsCounter;
+    projectsCounter++;
+    container.appendChild(clone);
+  }
 }
 
+let textArea = document.getElementById("infoShort");
+let characterCounter = document.getElementById("infoShortCharCount");
+const maxNumOfChars = 100;
 
+function changeColor(counter) {
+  if (counter < 15) {
+    characterCounter.style.color = "red";
+  } else if (counter < 50) {
+    characterCounter.style.color = "orange";
+  } else {
+    characterCounter.style.color = "";
+  }
+}
+
+const countCharacters = () => {
+  let numOfEnteredChars = textArea.value.length;
+  let counter = maxNumOfChars - numOfEnteredChars;
+  characterCounter.textContent = counter + "/100";
+  changeColor(counter);
+};
+
+textArea.addEventListener("input", countCharacters);
